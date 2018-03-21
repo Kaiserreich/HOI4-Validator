a2d5ac9933e1201c943d096e8d2decdf8f4323f4
roparex <edy_dobos@yahoo.com>
2018-03-21 19:05:27 +0200

    Changed check for old generals to check national focuses and scripted effects, also made it work for every mod not just KR.

---
+++ Scripts/checkForOldGenerals.py
@@ -0,0 +1,41 @@
+from os import listdir
+from codecs import open
+
+
+def check_for_old_generals(path, output_file):
+    check(path, output_file, "\\history\\countries", 'utf-8-sig')
+    check(path, output_file, "\\events", 'utf-8-sig')
+    check(path, output_file, "\\common\\national_focus", 'utf-8-sig')
+    check(path, output_file, "\\common\\scripted_effects", 'utf-8-sig')
+
+
+def check(path, output_file, sub_path, encoding):
+    path += sub_path
+    for filename in listdir(path):
+        current_line = 0
+        file = open(path+'\\'+filename, 'r', encoding)
+        line = file.readline()
+        current_line += 1
+        ok = 0
+        is_in_general = 0
+        while line:
+            if 'create_corps_commander' in line or 'create_field_marshal' in line:
+                is_in_general = 1
+            if "#mw thinks this is a land commander" in line:
+                output_file.write(sub_path + '\\' + filename +
+                                  " there's an unchanged KR general around line " + str(current_line) + '\n')
+            if "skill =" in line and "#" not in line and is_in_general == 1:
+                ok = 1
+            if "attack_skill" in line or "defense_skill" in line \
+                    or "planning_skill" in line or "logistics_skill" in line and ok == 1:
+                ok = 0
+                is_in_general = 0
+            if "}" in line and ok == 1:
+                output_file.write(sub_path + '\\' + filename +
+                                  " there's an old general around line " + str(current_line) + '\n')
+                is_in_general = 0
+                ok = 0
+            line = file.readline()
+            current_line += 1
+
+
