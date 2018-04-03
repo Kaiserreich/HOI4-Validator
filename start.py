66b66e77fcc33c8d9f399bc9ff0777b2492f8955
roparex <edy_dobos@yahoo.com>
2018-04-04 02:58:58 +0300

    Improved compatibility, seems Deli could not run it with the original command.

--- start.py
+++ start.py
@@ -2,9 +2,10 @@ import sys
 import os
 from codecs import open
 
-dirName = os.path.dirname(__file__)
+dirName = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(dirName + "\\Scripts")
 
+print (dirName + "\\Scripts\n")
 # imports go HERE
 from missingDivisionNamesGroup import missing_divisions_names_group
 from checkForOldGenerals import check_for_old_generals
