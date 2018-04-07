d9a49c38403093e143ccde61b7e646a55b5dc9f5
roparex <edy_dobos@yahoo.com>
2018-04-07 13:02:59 +0300

    Fixed looking in other language files.

--- Scripts/checkForDoubleLocs.py
+++ Scripts/checkForDoubleLocs.py
@@ -11,18 +11,19 @@ def check_for_double_locs(path, output_file):
         file = open(path + '\\' + filename, 'r', 'utf-8-sig')
         line = file.readline()
         current_line += 1
-        line = file.readline()
-        current_line += 1
-        while line:
-            line = line.strip()
-            i = line.find(':')
-            if i != -1:
-                loc = line[:i]
-                if loc in list_loc:
-                    output_file.write("Duplicated Loc: '" + loc + "' in: " + list_loc[loc] + " and in: '\\localisation\\" + filename + "' at line " + str(current_line) + "\n")
-                else:
-                    list_loc[loc] = "'\\localisation\\" + filename + "' at line " + str(current_line)
-                if "  " in line[i+1:]:
-                    output_file.write("Double Spaces in Loc: '" + loc + "' in: '\\localisation\\" + filename + "' at line " + str(current_line) + "\n")
+        if "l_english" in line:
             line = file.readline()
             current_line += 1
+            while line:
+                line = line.strip()
+                i = line.find(':')
+                if i != -1:
+                    loc = line[:i]
+                    if loc in list_loc:
+                        output_file.write("Duplicated Loc: '" + loc + "' in: " + list_loc[loc] + " and in: '\\localisation\\" + filename + "' at line " + str(current_line) + "\n")
+                    else:
+                        list_loc[loc] = "'\\localisation\\" + filename + "' at line " + str(current_line)
+                    if "  " in line[i+1:]:
+                        output_file.write("Double Spaces in Loc: '" + loc + "' in: '\\localisation\\" + filename + "' at line " + str(current_line) + "\n")
+                line = file.readline()
+                current_line += 1
