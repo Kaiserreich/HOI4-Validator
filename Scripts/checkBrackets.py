0bd9fb0bdf417fa6bff8986d0855bc6ab19a26d1
Gabriel Pessoa <gap@cin.ufpe.br>
2018-04-07 06:45:31 -0300

    Fixing false positives

--- Scripts/checkBrackets.py
+++ Scripts/checkBrackets.py
@@ -7,17 +7,6 @@ def check_brackets(path, output_file):
     check(path, output_file, "\\common\\national_focus", 'utf-8-sig')
 
 
-def not_commented(line):
-    for letter in line:
-        if letter == ' ' or letter == '\t':
-            continue
-        elif letter == '#':
-            return False
-        else:
-            return True
-    return False
-
-
 def check(path, output_file, sub_path, encoding):
     path += sub_path
     for filename in listdir(path):
@@ -27,25 +16,26 @@ def check(path, output_file, sub_path, encoding):
         current_line += 1
         stack = []
         while line:
-            if not_commented(line):
-                for letter in line:
-                    if letter == '(':
-                        stack.append(')')
-                    elif letter == '[':
-                        stack.append(']')
-                    elif letter == '{':
-                        stack.append('}')
-                    elif letter == ')' or letter == ']' or letter == '}':
-                        if (len(stack) == 0):
+            for letter in line:
+                if letter == '#':
+                    break
+                elif letter == '(':
+                    stack.append(')')
+                elif letter == '[':
+                    stack.append(']')
+                elif letter == '{':
+                    stack.append('}')
+                elif letter == ')' or letter == ']' or letter == '}':
+                    if (len(stack) == 0):
+                        output_file.write(
+                            sub_path + '\\' + filename + " Brackets: Not expected '" + letter + "' around line " + str(
+                                current_line) + '\n')
+                    else:
+                        letterPop = stack.pop()
+                        if (letterPop != letter):
                             output_file.write(
-                                sub_path + '\\' + filename + " Brackets: Not expected '" + letter + "' around line " + str(
+                                sub_path + '\\' + filename + " Brackets: Expecting: '" + letterPop + "' but found '" + letter + "' around line " + str(
                                     current_line) + '\n')
-                        else:
-                            letterPop = stack.pop()
-                            if (letterPop != letter):
-                                output_file.write(
-                                    sub_path + '\\' + filename + " Brackets: Expecting: '" + letterPop + "' but found '" + letter + "' around line " + str(
-                                        current_line) + '\n')
             line = file.readline()
             current_line += 1
         if len(stack) > 0:
--- Scripts/checkForDoubleLocs.py
+++ Scripts/checkForDoubleLocs.py
@@ -2,16 +2,7 @@ from os import listdir
 from codecs import open
 
 
-def find_first(line):
-    for i in range(0, len(line)):
-        if line[i] != ' ' and line[i] != '\t':
-            return i
-
-def check_double_spaces(str):
-    for i in range(0, len(str) - 1):
-        if(str[i] == ' ' and str[i+1] == ' '):
-            return True
-    return False
+ 
 def check_for_double_locs(path, output_file):
     path += "\\localisation"
     list_loc = {}
@@ -23,15 +14,15 @@ def check_for_double_locs(path, output_file):
         line = file.readline()
         current_line += 1
         while line:
+            line = line.strip()
             i = line.find(':')
             if i != -1:
-                j = find_first(line)
-                loc = line[j:i]
+                loc = line[:i]
                 if loc in list_loc:
                     output_file.write("Duplicated Loc: '" + loc + "' in: " + list_loc[loc] + " and in: '\\localisation\\" + filename + "' at line " + str(current_line) + "\n")
                 else:
                     list_loc[loc] = "'\\localisation\\" + filename + "' at line " + str(current_line)
-                if(check_double_spaces(line[i+1:])):
+                if "  " in line[i+1:]:
                     output_file.write("Double Spaces in Loc: '" + loc + "' in: '\\localisation\\" + filename + "' at line " + str(current_line) + "\n")
             line = file.readline()
             current_line += 1
