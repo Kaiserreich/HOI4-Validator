1c3c7223463436d2d515cc37e4dbc4f717f8940d
DrNjitram <martijn.gobes@gmail.com>
2018-04-09 21:17:30 +0200

    -Add support for multiple command line arguments, right now the first is the mod path, the second the hoi4 installation path.
    Seperate with -- ie: --path1 --path2

--- start.py
+++ start.py
@@ -15,27 +15,31 @@ from checkGFX import check_for_missing_gfx
 
 # Creating the path for the Mod
 ok = 0
-path = sys.argv[1]
+cpath = sys.argv[1]
+
 for string in sys.argv:
     if ok < 2:
         ok += 1
     else:
-        path += ' ' + string
+        cpath += ' ' + string
+        
+path = cpath.split("--")[1].strip()
+hoi4_path = cpath.split("--")[2].strip()
 
 # output file initialisation
 outputFile = open("validator.txt", 'w', 'utf-8-sig')
 
 # functions go HERE
 
-missing_divisions_names_group(path, outputFile)
-check_for_old_generals(path, outputFile)
-check_for_name_lists(path, outputFile)
-check_brackets(path, outputFile)
-check_for_double_locs(path, outputFile)
-check_for_missing_gfx(path, outputFile)
+#missing_divisions_names_group(path, outputFile)
+#check_for_old_generals(path, outputFile)
+#check_for_name_lists(path, outputFile)
+#check_brackets(path, outputFile)
+#check_for_double_locs(path, outputFile)
+check_for_missing_gfx(path, outputFile, hoi4_path)
 
 print ('The validator finished, the output file should be at ' + os.path.abspath(outputFile.name))
-#test
+
 
 
 
