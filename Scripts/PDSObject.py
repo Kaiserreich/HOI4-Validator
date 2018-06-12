be4058cb54b650903c243a8cc4340eed7eee4d3a
Antoni Baum <antoni.baum@gmail.com>
2018-06-13 01:07:43 +0200

    Add PDSScriptedEffect, PDSScriptedTrigger, PDSFocus, PDSFocusTree

--- Scripts/PDSObject.py
+++ Scripts/PDSObject.py
@@ -5,7 +5,7 @@ class PDSEvent:
     def __init__(self, event_id, event_type, title, desc, picture, is_triggered_only, fire_only_once, hidden, mean_time_to_happen, trigger, immediate, options):
         self.event_id = event_id
         self.event_type = event_type
-        self.namespace = re.search(".*(?=\.)", event_id).group()
+        self.namespace = re.search(r".*(?=\.)", event_id).group()
         self.title = title
         self.desc = desc
         self.picture = picture
@@ -22,19 +22,59 @@ class PDSEvent:
         self.immediate = immediate
         self.options = options
         self.references = []
-'''
-class PDSScriptedEvent:
-    #TODO
+
+class PDSScriptedEffect:
+    def __init__(self, name, effect):
+        self.name = name
+        self.effect = effect
 
 class PDSScriptedTrigger:
-    #TODO
+    def __init__(self, name, trigger):
+        self.name = name
+        self.trigger = trigger
+
 
 class PDSFocus:
-    #TODO
+    def __init__(self, focus_id, is_shared, focus_tree, icon, prerequisite, mutually_exclusive, available, bypass, allow_branch, x, y, relative_position_id, offset, cost, ai_will_do, completion_reward, completion_tooltip, cancel_if_invalid, continue_if_invalid, available_if_capitulated):
+        self.focus_id = focus_id
+        self.is_shared = is_shared
+        self.focus_tree = focus_tree
+        self.icon = icon
+        self.prerequisite = prerequisite
+        if prerequisite:
+            for idx, item in enumerate(prerequisite):
+                prerequisite[idx] = [x[2] for x in item]
+        self.mutually_exclusive = mutually_exclusive
+        if mutually_exclusive:
+            mutually_exclusive = [x[2] for x in mutually_exclusive]
+        self.available = available
+        self.bypass = bypass
+        self.allow_branch = allow_branch
+        self.x = x
+        self.real_x = x
+        self.y = y
+        self.real_y = y
+        self.relative_position_id = relative_position_id
+        self.offset = offset
+        self.ai_will_do = ai_will_do
+        self.completion_reward = completion_reward
+        self.completion_tooltip = completion_tooltip
+        self.cancel_if_invalid = cancel_if_invalid
+        self.continue_if_invalid = continue_if_invalid
+        self.available_if_capitulated = available_if_capitulated
+
 
 class PDSFocusTree:
-    #TODO
-
+    def __init__(self, focus_tree_id, country, default, continuous_focus_position):
+        self.focus_tree_id = focus_tree_id
+        self.country = country
+        self.default = default
+        self.continuous_focus_position = continuous_focus_position
+        if continuous_focus_position:
+            continuous_focus_position = (next(item[2] for item in continuous_focus_position if len(item) == 3 and item[0] == "x"), next(item[2] for item in continuous_focus_position if len(item) == 3 and item[0] == "y"))
+        self.focuses = dict()
+        self.shared_focuses = set()
+'''
 class PDSDecision:
     #TODO
 
@@ -53,7 +93,7 @@ class PDSIdea:
 class PDSGFX:
     #TODO
 
-class PDSLocalisation:
+class PDSScriptedLocalisation:
     #TODO
 
 class PDSUnitLeader:
--- Scripts/parsePDS.py
+++ Scripts/parsePDS.py
@@ -10,9 +10,12 @@ from PDSObject import *
 from os import listdir
 from openFile import open_file
 
+def make_string_lowercase(s,l,t):
+    return t[0].lower()
+
 def parse_PDS_script(string): 
     allowed_chars = srange(r"[':a-zA-Z0-9._-]")
-    EQUAL = Literal("=").suppress()
+    EQUAL = Literal("=")
     OPERATOR = oneOf("< > =")
     LBRACE = Literal("{").suppress()
     RBRACE = Literal("}").suppress()
@@ -30,25 +33,45 @@ def parse_PDS_script(string):
     pds_object << (pds_string("pds_quoted_argument") | pds_name("pds_agrument") | pds_list("pds_list"))
     pds_value << (pds_string("pds_quoted_list_item") | pds_name("pds_list_item"))
     pds_numerical_or_value = ((OPERATOR("pds_operator") + (pds_numerical("pds_numerical") ^ pds_date("pds_date"))) ^ (EQUAL + pds_object))
-    pds_member << Group(pds_name("pds_command") + pds_numerical_or_value)
+    pds_member << Group(pds_name("pds_command").setParseAction(make_string_lowercase) + pds_numerical_or_value)
     pds_members << OneOrMore(pds_member)
     pds_list_members << (pds_members | ZeroOrMore(pds_value))
 
     pds_members.ignore(pythonStyleComment)
+    pds_members.enablePackrat()
 
     string = pds_members.parseString(string, True)
     return(string)
 
+def parse_PDS_localisation(string):
+    allowed_chars = srange(r"['a-zA-Z0-9._-]")
+    pds_loc = Word(allowed_chars) + (":" + Word(nums)).suppress() + dblQuotedString()
+    pds_loc.setDebug()
+    pds_loc.ignore(pythonStyleComment)
+    string = pds_loc.parseString(string, True)
+    return(string)
+
 def get_triggered_text(triggered_text):
-    triggered_text = triggered_text[1]
+    triggered_text = triggered_text[2]
     if isinstance(triggered_text, str):
         return triggered_text
-    return next(item[1] for item in triggered_text if len(item) == 2 and item[0] == "text")
+    return next(item[2] for item in triggered_text if len(item) == 3 and item[0] == "text")
 
 def get_true_false_from_yes_no(string):
-    return True if string.lower() == "yes" else False
+    return True if string == "yes" else False
 
-def parse_event_file(event_file):
+def parse_localisation(loc_file, loc_dict):
+    print("Reading file %s" % loc_file)
+    loc_file = open_file(loc_file).read()
+
+    if not loc_file.strip():
+        return
+
+    parsed_loc_file = parse_PDS_localisation(loc_file)
+    for item in [x for x in parsed_loc_file]:
+        loc_dict[item[0]] = item[1]
+
+def parse_event_file(event_file, events_dict):
     namespaces = set()
 
     print("Reading file %s" % event_file)
@@ -58,43 +81,125 @@ def parse_event_file(event_file):
         return
 
     parsed_event_file = parse_PDS_script(event_file)
-    for item in [x[1] for x in parsed_event_file if (len(x) == 2 and x[0].lower() == "add_namespace")]:
+    for item in [x[2] for x in parsed_event_file if (len(x) == 3 and x[0]== "add_namespace")]:
         namespaces.add(item)
 
-    text_events = [x for x in parsed_event_file if (x and x[0].lower() == "country_event")]
-    events = dict()
+    text_events = [x for x in parsed_event_file if (x and x[0] == "country_event")]
     for event in text_events:
         if not event:
             continue
         event_type = event[0]
         event = event[1]
-        '''
-        print("id = \t" + str(next(item[1] for item in event if len(item) == 2 and item[0].lower() == "id")))
-        print("title = \t" + str([get_triggered_text(item) for item in event if len(item) == 2 and item[0].lower() == "title"]))
-        print("picture = \t" + str(next((item[1] for item in event if len(item) == 2 and item[0].lower() == "picture"), None)))
-        print("is_triggered_only = \t" + str(next((get_true_false_from_yes_no(item[1]) for item in event if len(item) == 2 and item[0] == "is_triggered_only"), False)))
-        print("fire_only_once = \t" + str(next((get_true_false_from_yes_no(item[1]) for item in event if len(item) == 2 and item[0] == "fire_only_once"), False)))
-        print("hidden = \t" + str(next((get_true_false_from_yes_no(item[1]) for item in event if len(item) == 2 and item[0] == "hidden"), False)))
-        print("mean_time_to_happen = \t" + str(next((item[1] for item in event if len(item) == 2 and item[0] == "mean_time_to_happen"), None)))
-        print("trigger = \t" + str(next((item[1] for item in event if len(item) == 2 and item[0] == "trigger"), None)))
-        print("immediate = \t" + str(next((item[1] for item in event if len(item) == 2 and item[0] == "immediate"), None)))
-        print("options = \t" + str([item[1] for item in event if len(item) == 2 and item[0] == "option"]))
-        '''
+        
         event = PDSEvent(
-            event_id=next(item[1] for item in event if len(item) == 2 and item[0] == "id"),
+            event_id=next(item[2] for item in event if len(item) == 3 and item[0] == "id"),
             event_type=event_type,
-            title=[get_triggered_text(item) for item in event if len(item) == 2 and item[0] == "title"],
-            desc=[get_triggered_text(item) for item in event if len(item) == 2 and item[0] == "desc"],
-            picture=next((item[1] for item in event if len(item) == 2 and item[0].lower() == "picture"), None),
-            is_triggered_only=next((get_true_false_from_yes_no(item[1]) for item in event if len(item) == 2 and item[0] == "is_triggered_only"), False),
-            fire_only_once=next((get_true_false_from_yes_no(item[1]) for item in event if len(item) == 2 and item[0] == "fire_only_once"), False),
-            hidden=next((get_true_false_from_yes_no(item[1]) for item in event if len(item) == 2 and item[0] == "hidden"), False),
-            mean_time_to_happen=next((item[1] for item in event if len(item) == 2 and item[0] == "mean_time_to_happen"), None),
-            trigger=next((item[1] for item in event if len(item) == 2 and item[0] == "trigger"), None),
-            immediate=next((item[1] for item in event if len(item) == 2 and item[0] == "immediate"), None),
-            options=[item[1] for item in event if len(item) == 2 and item[0] == "option"]
+            title=[get_triggered_text(item) for item in event if len(item) == 3 and item[0] == "title"],
+            desc=[get_triggered_text(item) for item in event if len(item) == 3 and item[0] == "desc"],
+            picture=next((item[2] for item in event if len(item) == 3 and item[0] == "picture"), None),
+            is_triggered_only=next((get_true_false_from_yes_no(item[2]) for item in event if len(item) == 3 and item[0] == "is_triggered_only"), False),
+            fire_only_once=next((get_true_false_from_yes_no(item[2]) for item in event if len(item) == 3 and item[0] == "fire_only_once"), False),
+            hidden=next((get_true_false_from_yes_no(item[2]) for item in event if len(item) == 3 and item[0] == "hidden"), False),
+            mean_time_to_happen=next((item[2] for item in event if len(item) == 3 and item[0] == "mean_time_to_happen"), None),
+            trigger=next((item[2] for item in event if len(item) == 3 and item[0] == "trigger"), None),
+            immediate=next((item[2] for item in event if len(item) == 3 and item[0] == "immediate"), None),
+            options=[item[2] for item in event if len(item) == 3 and item[0] == "option"]
         )
         if not event.namespace in namespaces:
             raise ValueError('Event %s namespace %s is not in file\'s namespaces.' % (event.event_id, event.namespace))
-        events[event.event_id] = event
-    print(events)
+        events_dict[event.event_id] = event
+    print(events_dict)
+
+def parse_scripted_effect_file(scripted_effect_file, scripted_effects_dict):
+
+    print("Reading file %s" % scripted_effect_file)
+    scripted_effect_file = open_file(scripted_effect_file).read()
+
+    if not scripted_effect_file.strip():
+        return
+
+    parsed_scripted_effect_file = parse_PDS_script(scripted_effect_file)
+    for item in [x for x in parsed_scripted_effect_file]:
+        scripted_effect = PDSScriptedEffect(item[0], item[2])
+        scripted_effects_dict[item[0]] = scripted_effect
+
+def parse_scripted_trigger_file(scripted_trigger_file, scripted_triggers_dict):
+
+    print("Reading file %s" % scripted_trigger_file)
+    scripted_trigger_file = open_file(scripted_trigger_file).read()
+
+    if not scripted_trigger_file.strip():
+        return
+
+    parsed_scripted_trigger_file = parse_PDS_script(scripted_trigger_file)
+    for item in [x for x in parsed_scripted_trigger_file]:
+        scripted_trigger = PDSScriptedTrigger(item[0], item[2])
+        scripted_triggers_dict[item[0]] = scripted_trigger
+
+def parse_focus_file(focus_file, focuses_dict, focus_tree_dict):
+    
+    print("Reading file %s" % focus_file)
+    focus_file = open_file(focus_file).read()
+
+    if not focus_file.strip():
+        return
+
+    parsed_focus_file = parse_PDS_script(focus_file)
+    #print(parsed_focus_file)
+
+    for item in parsed_focus_file:
+        #print(item)
+        if item[0] == 'focus_tree':
+            focus_tree_id = next(item_2[2] for item_2 in item[2] if len(item_2) == 3 and item_2[0] == "id")
+            focus_tree = PDSFocusTree(
+                focus_tree_id=focus_tree_id,
+                country=next(item_2[2] for item_2 in item[2] if len(item_2) == 3 and item_2[0] == "country"),
+                default=next((get_true_false_from_yes_no(item_2[2]) for item_2 in item[2] if len(item_2) == 3 and item_2[0] == "default"), False),
+                continuous_focus_position=next((item_2[2] for item_2 in item[2] if len(item_2) == 3 and item_2[0] == "continuous_focus_position"), None)
+            )
+            focus_tree_dict[focus_tree_id] = focus_tree
+            for item in [x for x in item[2] if (x and x[0] == "focus")]:
+                focus = create_focus(item, focus_tree_id)
+                focuses_dict[focus.focus_id] = focus
+                focus_tree.focuses[focus.focus_id] = focus
+                print(focus)
+            for item in [x for x in item[2] if (x and x[0] == "shared_focus")]:
+                focus_tree.shared_focuses.add(item[2])
+        elif item[0] == 'focus' or item[0] == 'shared_focus':
+            try:
+                focus = create_focus(item[0], None)
+                focuses_dict[focus.focus_id] = focus
+            except ValueError:
+                #handle somehow
+                pass
+
+def create_focus(parsed_focus, focus_tree):
+    is_shared = parsed_focus[0] == 'shared_focus'
+    if is_shared and not focus_tree:
+        raise ValueError('focus %s is not shared and outside a focus tree' % next(item[2] for item in parsed_focus if len(item) == 3 and item[0] == "id"))
+    parsed_focus = parsed_focus[2]
+    focus = PDSFocus(
+        focus_id=next(item[2] for item in parsed_focus if len(item) == 3 and item[0] == "id"),
+        is_shared=is_shared,
+        focus_tree=focus_tree,
+        cost=next(item[2] for item in parsed_focus if len(item) == 3 and item[0] == "cost"),
+        icon=next((item[2] for item in parsed_focus if len(item) == 3 and item[0] == "icon"), None),
+        prerequisite=[item[2] for item in parsed_focus if len(item) == 3 and item[0] == "prerequisite"],
+        mutually_exclusive=next((item[2] for item in parsed_focus if len(item) == 3 and item[0] == "mutually_exclusive"), None),
+        available=next((item[2] for item in parsed_focus if len(item) == 3 and item[0] == "available"), None),
+        bypass=next((item[2] for item in parsed_focus if len(item) == 3 and item[0] == "bypass"), None),
+        allow_branch=next((item[2] for item in parsed_focus if len(item) == 3 and item[0] == "allow_branch"), None),
+        x=next(item[2] for item in parsed_focus if len(item) == 3 and item[0] == "x"),
+        y=next(item[2] for item in parsed_focus if len(item) == 3 and item[0] == "y"),
+        relative_position_id=next((item[2] for item in parsed_focus if len(item) == 3 and item[0] == "relative_position_id"), None),
+        offset=next((item[2] for item in parsed_focus if len(item) == 3 and item[0] == "offset"), None),
+        ai_will_do=next((item[2] for item in parsed_focus if len(item) == 3 and item[0] == "ai_will_do"), None),
+        completion_reward=next((item[2] for item in parsed_focus if len(item) == 3 and item[0] == "completion_reward"), None),
+        completion_tooltip=next((item[2] for item in parsed_focus if len(item) == 3 and item[0] == "completion_tooltip"), None),
+        cancel_if_invalid=next((get_true_false_from_yes_no(item[2]) for item in parsed_focus if len(item) == 3 and item[0] == "cancel_if_invalid"), False),
+        continue_if_invalid=next((get_true_false_from_yes_no(item[2]) for item in parsed_focus if len(item) == 3 and item[0] == "continue_if_invalid"), False),
+        available_if_capitulated=next((get_true_false_from_yes_no(item[2]) for item in parsed_focus if len(item) == 3 and item[0] == "available_if_capitulated"), False),
+    )
+    return focus
+
+parse_focus_file("national_focus/KR_Brazil.txt", None, None)
