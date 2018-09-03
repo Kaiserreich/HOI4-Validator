dc847dee4a78407bbfef52f6b9c3a5843120a3ab
Khalil Sheehan-Miles <sheehanmilesk@wit.edu>
2018-09-03 19:25:53 -0400

    Fixed checkfordoublelocs
    Make sure to proof your for filename in listdir(path)s for subfolders people!

--- Scripts/checkForDoubleLocs.py
+++ Scripts/checkForDoubleLocs.py
@@ -16,26 +16,45 @@ def check_and_strip(string, _range):
 def check_for_double_locs(path, output_file):
     path += "\\localisation"
     list_loc = {}
+    subfolderlist = []
     for filename in listdir(path):
-        current_line = 0
-        file = open_file(path + '\\' + filename)
+        if '.yml' in filename:
+            file = open_file(path + '\\' + filename)
+            list_loc = linecheck(file, output_file, filename, list_loc, '')
+        else:
+            subfolderlist.append(filename)
+    for subfolder in subfolderlist:
+        newpath = path+ '\\' + subfolder
+        for filename in listdir(newpath):
+            if '.yml' in filename:
+                file = open_file(newpath + '\\' + filename)
+                list_loc = linecheck(file, output_file, filename, list_loc, subfolder)
+
+def linecheck(file, output_file, filename, list_loc, subfolder):
+    if subfolder != '':
+        subfolder = subfolder + '\\'
+    current_line = 0
+    line = file.readline()
+    current_line += 1
+    if "l_english" in line:
         line = file.readline()
         current_line += 1
-        if "l_english" in line:
+        while line:
+            line = line.strip()
+            i = line.find(':')
+            if i != -1 and line[0] != '#':
+                loc = line[:i]
+                if loc in list_loc:
+                    output_file.write("Duplicated Loc: '" + loc + "' in: " + list_loc[
+                        loc] + " and in: '\\localisation\\" + subfolder +filename + "' at line " + str(current_line) + "\n")
+                else:
+                    list_loc[loc] = "'\\localisation\\" + subfolder + filename + "' at line " + str(current_line)
+                # if "  " in line[i+1:]:
+                if check_and_strip(line, i) is True:
+                    # print("Double Spaces in Loc: '" + loc + "' in: '\\localisation\\" + filename + "' at line " + str(current_line))
+                    output_file.write(
+                        "Double Spaces in Loc: '" + loc + "' in: '\\localisation\\" + filename + "' at line " + str(
+                            current_line) + "\n")
             line = file.readline()
             current_line += 1
-            while line:
-                line = line.strip()
-                i = line.find(':')
-                if i != -1 and line[0] != '#':
-                    loc = line[:i]
-                    if loc in list_loc:
-                        output_file.write("Duplicated Loc: '" + loc + "' in: " + list_loc[loc] + " and in: '\\localisation\\" + filename + "' at line " + str(current_line) + "\n")
-                    else:
-                        list_loc[loc] = "'\\localisation\\" + filename + "' at line " + str(current_line)
-                    #if "  " in line[i+1:]:
-                    if check_and_strip(line, i) is True:
-                        #print("Double Spaces in Loc: '" + loc + "' in: '\\localisation\\" + filename + "' at line " + str(current_line))
-                        output_file.write("Double Spaces in Loc: '" + loc + "' in: '\\localisation\\" + filename + "' at line " + str(current_line) + "\n")
-                line = file.readline()
-                current_line += 1
+    return list_loc
