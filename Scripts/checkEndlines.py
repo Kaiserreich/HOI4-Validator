6921d8b2e4fc356fbd4aa85270e9fdf426e2a840
BlizzardOfOzzy <ojd4hh@virginia.edu>
2018-09-06 23:27:35 -0400

    Changed newline check to actually look for bad newlines within localisation text

--- Scripts/checkEndlines.py
+++ Scripts/checkEndlines.py
@@ -1,17 +1,13 @@
-import bisect
 from os import listdir
 from os.path import isdir
 from timedFunction import timed
 from openFile import open_file
 from removeComments import remove_comments
-from findNewlineIndices import find_indices_of_new_lines
 
 
 @timed
 def check_endlines(path, output_file):
-    check(path, output_file, "\\events")
-    check(path, output_file, "\\common")
-    check(path, output_file, "\\history")
+    check(path, output_file, "\\localisation")
 
 
 def check(path, output_file, sub_path):
@@ -24,18 +20,22 @@ def check(path, output_file, sub_path):
         file = open_file(path + '\\' + filename)
         string = file.read()
         string_without_comments = remove_comments(string)
-        indices_of_newlines_without_comments = find_indices_of_new_lines(string_without_comments)
+        within_quotes = False
+        current_line = 1
         for i in range(0, len(string_without_comments)):
-            if string_without_comments[i:i+2] == '/n' or string_without_comments[i:i+2] == '\\N':
-                line = bisect.bisect(indices_of_newlines_without_comments, i) + 1
-                output_file.write(sub_path + '\\' + filename + ' improper newline on line ' + str(line) + '\n')
-
-        indices_of_newlines = find_indices_of_new_lines(string)
-        for current_line, index in enumerate(indices_of_newlines):
-            if string[index - 1] == ' ':
-                output_file.write(sub_path + '\\' + filename + ' space before newline on line ' + str(current_line+1) + '\n')
-            if index < len(string) - 2:
-                if string[index + 1] == ' ' and string[index + 2] != ' ':
-                    output_file.write(sub_path + '\\' + filename + ' space after newline on line ' + str(current_line+1) + '\n')
+            if string_without_comments[i] == "\"":
+                within_quotes = not within_quotes
+            if not within_quotes:
+                if string_without_comments[i] == '\n':
+                    current_line += 1
+            if within_quotes:
+                if string_without_comments[i:i+2] == '/n' or string_without_comments[i:i+2] == '\\N':
+                    output_file.write(sub_path + '\\' + filename + ' improper newline on line ' + str(current_line) + '\n')
+                if string_without_comments[i:i+2] == '\\n':
+                    if string_without_comments[i - 1] == ' ':
+                        output_file.write(sub_path + '\\' + filename + ' space before newline on line ' + str(current_line) + '\n')
+                    if i < len(string_without_comments) - 1:
+                        if string_without_comments[i + 2] == ' ':
+                            output_file.write(sub_path + '\\' + filename + ' space after newline on line ' + str(current_line) + '\n')
 
 
