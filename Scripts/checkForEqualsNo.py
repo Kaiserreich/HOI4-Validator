ee7a660003c8543649c4c4a265d245bed7e44252
Khalil Sheehan-Miles <sheehanmilesk@wit.edu>
2018-09-06 21:54:25 -0400

    Oh dear I forgot to commit something important in the last commit
    I'm dumb.

--- Scripts/checkForEqualsNo.py
+++ Scripts/checkForEqualsNo.py
@@ -1,22 +1,47 @@
 from createDict import create_search_dict
 from timedFunction import timed
+from openFile import open_file
+from os import listdir
+
 
 @timed
+
 def check_for_equals_no(path, output_file):
-    #this currently searches in the wrong places because I don't know where to search. Someone tell me where to search.
     originalpath = path
-    #this creates a dict of all the generals that are referenced
     nodict = {}
     linedict = {}
     filedict = {}
     searchstrings = ['= no']
     filterstrings = []
     thingstripped = '=no'
+    triggerlist = find_scripted_triggers(originalpath)
     path = originalpath + "\\common\\national_focus"
-    nodict, linedict, filedict = create_search_dict(nodict, linedict, filedict, path, searchstrings, filterstrings, thingstripped)
+    nodict, linedict, filedict = create_search_dict(nodict, linedict, filedict, path, searchstrings, filterstrings, thingstripped, searchstrings2 = triggerlist)
     path = originalpath + "\\common\\decisions"
-    nodict, linedict, filedict = create_search_dict(nodict, linedict, filedict, path, searchstrings, filterstrings, thingstripped)
+    nodict, linedict, filedict = create_search_dict(nodict, linedict, filedict, path, searchstrings, filterstrings, thingstripped, searchstrings2 = triggerlist)
     for key in nodict:
         result = "= no is used in file " + filedict[key] + " on line " + str(linedict[key]) + ". Don't do that.\n"
-        #print(result)
+        print(result)
         output_file.write(result)
+
+
+def find_scripted_triggers(path):
+    path += "\\common\\scripted_triggers"
+    triggerlist = []
+    print("finding scripted triggers")
+    for filename in listdir(path):
+        #print(filename)
+        if '.txt' in filename:
+            file = open_file(path + "\\" + filename)
+            depth = 0;
+            for line in file:
+                if depth == 0 and '=' in line:
+                    trigger = line.split('=')[0].strip()
+                    if ("#" in trigger) == False:
+                        #print(trigger)
+                        triggerlist.append(trigger)
+                if '{' in line:
+                    depth = depth + 1
+                if '}' in line:
+                    depth = depth - 1
+    return triggerlist
